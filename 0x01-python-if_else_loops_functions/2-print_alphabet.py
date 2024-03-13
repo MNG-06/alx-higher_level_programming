541e0d6a6e2fc273372956372d409745d6c2d8c5
MNG-06 <123241772+MNG-06@users.noreply.github.com>
2024-03-13 03:23:27 +0200

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 2-print_alphabet.py
-# Brennan D Baraban <375@holbertonschool.com>
+# MNG-06 <mphela.mminanare@gmail.com>
 
 """Print the alphabet in lowercase, not followed by a new line."""
 for letter in range(97, 123):
