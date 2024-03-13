2f05d28903be954d2c25dcb96d4fd7a5fedbce67
MNG-06 <123241772+MNG-06@users.noreply.github.com>
2024-03-13 03:25:55 +0200

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 # 9-print_last_digit.py
-# Brennan D Baraban <375@holbertonschool.com>
-
+# MNG-06 <mphela.mminanare@gmail.com>
 
 def print_last_digit(number):
     """Print the last digit of a number and return it."""
